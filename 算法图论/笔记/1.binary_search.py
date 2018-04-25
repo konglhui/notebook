227d4603cded5efc2ae60d2df984e9fe5a3b1b05
konglhui <1135984111@qq.com>
2018-04-25 17:36:14 +0800

    刚开始

---
+++ 算法图论/笔记/1.binary_search.py
@@ -0,0 +1,20 @@
+def binary_search(list,item):
+	low = 0
+	high = len(list) - 1
+
+	while low<= high:
+		median = (low + high)//2
+		guess = list[median]
+		if item = guess:
+			return median
+		elif item > guess:
+			low = median+1
+		elif item < guess:
+			high = median-1
+
+my_list = [1,2,3,4,5,6,7,8,9]
+
+print(binary_search(mylist,4))
+
+
+

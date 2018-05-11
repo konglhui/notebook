dd337b597117786eb02756b40005c907aa489b0d
konglhui <1135984111@qq.com>
2018-05-12 00:18:47 +0800

    第七章结束

--- 算法图论/笔记/chapter7/diks.py
+++ 算法图论/笔记/chapter7/diks.py
@@ -20,3 +20,51 @@ class Path(object):
         self.length = length_init
 
 
+def diks(graph):
+    optimal_path = Path([],0)#保存最优路径
+
+    nodes = graph["start"]
+    all_path = {}#保存所有节点的距离，进行比较
+    while nodes :
+        temp_length = 999
+        optimal_node = ' '
+        for node,path_length in nodes.items():
+            #保存所有节点的的路径和距离
+            temp_node = Path([],0)
+            for item in optimal_path.path:
+                temp_node.path.append(item)
+            temp_node.length += optimal_path.length
+            temp_node.path.append(node)
+            temp_node.length += path_length
+            if node in all_path.keys():
+                if all_path[node].length > optimal_path.length+path_length:
+                    all_path[node] = temp_node
+            else:
+                all_path[node] = temp_node
+            #选择最优距离
+            if path_length < temp_length:
+                optimal_node = node
+                temp_length = path_length
+
+
+        optimal_path.length += temp_length
+        optimal_path.path.append(optimal_node)
+        
+        #判断是否有更优的节点
+        if all_path[optimal_path.path[-1]].length < optimal_path.length:
+            optimal_path.length =  all_path[optimal_path.path[-1]].length
+            for item in all_path[optimal_path.path[-1]].path:
+                optimal_path.path.append(item)
+            
+
+        nodes = graph[optimal_node]
+    
+    for item in optimal_path.path:
+        print(item)
+
+    print(optimal_path.length)
+
+diks(graph)
+
+
+

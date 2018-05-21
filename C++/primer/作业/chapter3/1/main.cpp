fe3ecbbd46277e991c39b60855e587574a0eb1b8
konglhui <1135984111@qq.com>
2018-05-21 17:32:48 +0800

    写了点

---
+++ C++/primer/作业/chapter3/1/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    const int num = 12;
+    int number;
+    cout <<"Ó¢³ß×ª»»³ÉÓ¢´ç¡£\n"<< "ÄãÏëÒª×ª»»µÄ³ß´ç£º" << endl;
+    cin>>number;
+    number *= num;
+    cout<<number;
+    return 0;
+}

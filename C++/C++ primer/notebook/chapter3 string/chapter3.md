c1217ddd71aa7aa3b63aee988a20653c9209e964
konglhui <1135984111@qq.com>
2018-06-03 23:24:30 +0800

    第三章，还没有结束

--- C++/C++ primer/notebook/chapter3 string/chapter3.md
+++ C++/C++ primer/notebook/chapter3 string/chapter3.md
@@ -1,4 +1,9 @@
+# chapter 3
+
 ![mark](http://p6yio0wew.bkt.clouddn.com/blog/180529/kADF9FGK48.png)
 
 ![mark](http://p6yio0wew.bkt.clouddn.com/blog/180529/fGD81gleAF.png)
 
+![mark](http://p6yio0wew.bkt.clouddn.com/18-6-3/66917745.jpg)
+
+![mark](http://p6yio0wew.bkt.clouddn.com/18-6-3/65389497.jpg)
--- C++/C++ primer/test.cpp
+++ C++/C++ primer/test.cpp
@@ -1,7 +1,23 @@
 #include <iostream>
+#include<vector>
+using std::endl;
+using std::cin;
+using std::cout;
+using std::string;
+using std::vector;
+using std::toupper;
 
 int main()
 {
-    std::cout<<"hello word"<<std::endl;
+    string word;
+    vector<string> text;
+    while (cin>>word){
+        for (auto &c:word)
+            c = toupper(c);
+        text.push_back(word);
+    }
+    for (auto a:text){
+        cout<<a<<endl;
+    }
     return 0;
 }

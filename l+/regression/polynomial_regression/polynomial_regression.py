3a09d2ff78cf6b4aa62c148220bc25bc36880b6f
konglhui <1135984111@qq.com>
2018-06-04 09:41:24 +0800

    dont ubderstand

--- l+/regression/polynomial_regression/polynomial_regression.py
+++ l+/regression/polynomial_regression/polynomial_regression.py
@@ -3,23 +3,13 @@ import pandas as pd
 
 class polynomial_regression(object):
 
-    def __init__(self,polynomial_n = 2):
-        self.polynomial_n = polynomial_n        #本次需要的多项式的次数
+    def __init__(self):
+        pass
     #对模型进行训练
     def fit(self,x,y):
-        x_temp = self.feature_polynomil(x)
-        x_temp.loc[:,'intercept'] = 1
-        xx = x_temp.as_matrix()
+        xx = x.as_matrix()
         yy = y.as_matrix()
         self.w = np.dot(np.dot(np.linalg.pinv(np.dot(xx.T,xx)),xx.T),yy)
-
-    #对数据集进行n次转化
-    def feature_polynomil(self,x):
-        polynomial_x = x
-        for i in range(self.polynomial_n-1):
-            for column in polynomial_x.columns:
-                polynomial_x.loc[:,column + "_" + str(i)] = polynomial_x[column] ** (i+2)
-        return polynomial_x        
 
     #返回系数
     def coef_(self):
@@ -30,16 +20,13 @@ class polynomial_regression(object):
         return self.w[-1,:]
     #对数据进行预测
     def predict(self,test_x):
-        x_temp = self.feature_polynomil(test_x)
-        x_temp.loc[:,'intercept'] = 1
-        xx = x_temp.as_matrix()
+        xx = test_x.as_matrix()
         self.predict_y = np.dot(xx,self.w)
         return self.predict_y
 
     #对预测进行评估
     def mse(self,y_true):
-        n = len(y_true)
-        mse = sum((np.square(y_true-self.predict_y))/y_true)/n *100
+        mse = sum(np.square(y_true-self.predict_y)/len(y_true))
         return mse
 
 #对数据进行划分，可以数用sklearn里的数据划分
@@ -51,25 +38,31 @@ def train_test_split(x,y,train_size = 0.7):
     test_y = y[n:]
     return train_x,test_x,train_y,test_y
 
+
+#对数据集进行n次转化 
+def feature_polynomil(x,n = 2):
+    for i in range(1,n):
+        a = x.columns
+        for column in  a:
+            x[column + "-" + str(i)] = x.loc[:,column]**(i+1)
+    return x       
+
 def main():
     #获取数据
-    for i in range(2,10):
-        file = 'C:\\Users\\Administrator\\Desktop\\challenge-2-bitcoin.csv'
-        df = pd.read_csv(file)
-        feature = df[['btc_total_bitcoins','btc_transaction_fees']]
-        target = df['btc_market_price']
+    file = 'E:\\klinghui\\notebook\\l+\\regression\\polynomial_regression\\challenge-2-bitcoin.csv'
+    df = pd.read_csv(file)
+    feature = df[['btc_total_bitcoins','btc_transaction_fees']]
+    target = df['btc_market_price']
+    n_feature = feature_polynomil(feature,n = 5)
+    n_feature.dropna(inplace=True)
+    train_x,test_x,train_y,test_y = train_test_split(n_feature,target)
+    
+    model = polynomial_regression()
+    model.fit(train_x,train_y)
+    model.predict(test_x)
 
-        train_x,test_x,train_y,test_y = train_test_split(feature,target)
-        
-        model = polynomial_regression(i)
-        model.fit(train_x,train_y)
-        print("ceof:",model.coef_())
-        print("intercept",model.intercept_)
-
-        model.predict(test_x)
-
-        print("mse:",model.mse(test_y))
-        print('_________________________________')
+    print("mse:",model.mse(test_y))
+    print('_________________________________')
 
 if __name__ == "__main__":
     main() 
